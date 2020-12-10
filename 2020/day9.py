6b1cbc3b52af476b3e53cca94ea6a342fd2d3e28
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-12-10 14:23:22 -0500

    Optimize day 9.
    Signed-off-by: Chaichontat Sriworarat <chaichontat.s@duke.edu>

--- 2020/day9.py
+++ 2020/day9.py
@@ -1,33 +1,40 @@
 #%%
 from utils import load
 
-raw = load("day9.txt", parseint=True)[:-1]
+raw = load("day9.txt", parseint=True)
 
 # %% Part 1
 def run_test(idx):
-    for i in range(idx):
-        for j in range(i, idx):
+    for i in range(idx - 25, idx):
+        for j in range(i + 1, idx):
             if raw[idx] == raw[i] + raw[j]:
                 return True
     return False
 
 
+target = -1
+for idx in range(25, len(raw)):
+    if not run_test(idx):
+        target = idx
+        break
+
+
 def test1():
-    for idx in range(25, len(raw)):
-        if not run_test(idx):
-            assert raw[idx] == 1038347917
-            return idx
+    assert raw[target] == 1038347917
 
 
 # %%
-# Since sorted.
 def test2():
-    magic = test1()
-    for i in range(magic):
-        for j in range(i, magic):
-            if sum(raw[i:j]) == raw[magic]:
-                assert min(raw[i:j]) + max(raw[i:j]) == 137394018
-                return
+    cumsum = dict()  # Sum: index.
+    curr_sum = 0
+
+    for i in range(len(raw)):
+        cumsum[curr_sum] = i
+        curr_sum += raw[i]
+        if (start_sum := curr_sum - raw[target]) in cumsum:  # DP.
+            vals = raw[cumsum[start_sum] : i + 1]
+            assert min(vals) + max(vals) == 137394018
+            break
 
 
 # %%
