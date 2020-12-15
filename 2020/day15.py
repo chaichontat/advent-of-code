347863d96a80c82c997bdf980ef297b1ef7c43f1
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-12-15 12:55:45 -0500

    Day 15.
    Signed-off-by: Chaichontat Sriworarat <chaichontat.s@duke.edu>

---
+++ 2020/day15.py
@@ -0,0 +1,30 @@
+#%%
+from utils import load
+
+raw = load("day15.txt", split=",", parseint=True)
+
+# %%
+def run(stps):
+    if stps <= len(raw):
+        return raw[stps - 1]
+    elif stps < 1:
+        raise ValueError
+
+    last_spoke = {x: i for i, x in enumerate(raw, start=1)}  # n: last spoke
+    speaking = raw[-1]
+
+    for i in range(len(raw), stps):
+        last_spoke[speaking], speaking = i, i - last_spoke.get(speaking, i)
+
+    return speaking
+
+
+def test1():
+    assert run(2020) == 1015
+
+
+def test2():
+    assert run(int(3e7)) == 201
+
+
+# %%
