04c6c0a92f20aac2b0e7e8fa62bbbc60bac622b5
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-12-25 12:13:02 -0500

    🎄 Day 25.
    Signed-off-by: Chaichontat Sriworarat <chaichontat.s@duke.edu>

---
+++ 2020/day25.py
@@ -0,0 +1,34 @@
+#%%
+from collections import defaultdict
+
+from utils import load
+
+raw = load("day25.txt", parseint=True)
+
+# %%
+cache = defaultdict(lambda: [1])
+
+
+def transform(subject, loop_size):
+    cs = cache[subject]
+    try:
+        return cs[loop_size]
+    except IndexError:
+        while len(cs) < loop_size + 1:
+            cs.append(cs[-1] * subject % 20201227)
+    return cs[loop_size]
+
+
+def get_loop(subject, pub):
+    i = 1
+    while transform(subject, i) != pub:
+        i += 1
+    return i
+
+
+def test1():
+    loop_card = get_loop(7, raw[0])
+    assert transform(raw[1], loop_card) == 15217943
+
+
+#%%
