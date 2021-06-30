d69928a91ddfa4e01139ca6ed4ee3543d7a3be9b
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2021-06-29 22:03:51 -0400

    Day 2 cleanup.
    Signed-off-by: Chaichontat Sriworarat <chaichontat.s@duke.edu>

--- 2019/rust/src/day02.rs
+++ 2019/rust/src/day02.rs
@@ -2,13 +2,15 @@ use std::cmp::Ordering;
 
 use super::intcode::*;
 
-pub fn part1(raw: &[String]) -> usize {
-    let mut ic = IntCode::from(&raw[0]);
-    ic.mem[1] = 12;
-    ic.mem[2] = 2;
+fn run_ic(mem: &String, m1: isize, m2: isize) -> isize {
+    let mut ic = IntCode::from(mem);
+    ic.mem[1] = m1;
+    ic.mem[2] = m2;
     ic.run();
-    ic.mem[0] as usize
+    ic.mem[0]
 }
+
+pub fn part1(raw: &[String]) -> usize { run_ic(&raw[0], 12, 2) as usize }
 
 /// Initial 10×10 grid search indicates monotonicity of the intcode function.
 /// Higher gradient w.r.t mem[1]. Binary search on mem[1] first then mem[2].
@@ -35,24 +37,12 @@ fn fn_binary_search(
     }
 }
 
-fn run_ic(mem: &[isize], m1: isize, m2: isize) -> isize {
-    let mut ic = IntCode::from(mem);
-    ic.mem[1] = m1;
-    ic.mem[2] = m2;
-    ic.run();
-    ic.mem[0]
-}
-
 pub fn part2(raw: &[String]) -> usize {
     let target = 19690720;
-    let mem: Vec<isize> = raw[0]
-        .split(',')
-        .map(|x| x.parse::<isize>().unwrap())
-        .collect();
 
-    match fn_binary_search(|x| run_ic(&mem, x, 0), target, 0, 100) {
+    match fn_binary_search(|x| run_ic(&raw[0], x, 0), target, 0, 100) {
         Ok(r) => 100 * r as usize,
-        Err(r) => match fn_binary_search(|x| run_ic(&mem, r - 1, x), target, 0, 100) {
+        Err(r) => match fn_binary_search(|x| run_ic(&raw[0], r - 1, x), target, 0, 100) {
             Ok(t) => (100 * (r - 1) + t) as usize,
             Err(_) => panic!("Doesn't work."),
         },
