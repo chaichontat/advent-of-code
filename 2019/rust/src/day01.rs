13330f4804e8577ad200564ba74f8193e5ab107a
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2021-07-01 22:09:07 -0400

    Update Day 01.
    Signed-off-by: Chaichontat Sriworarat <chaichontat.s@duke.edu>

--- 2019/rust/src/day01.rs
+++ 2019/rust/src/day01.rs
@@ -1,8 +1,6 @@
-use super::utils::*;
-
-fn fuel(x: usize) -> Option<usize> { (x / 3).checked_sub(2) }
-
-pub fn part1(raw: &[String]) -> usize { raw.iter().map(|x| int(x)).map(fuel).flatten().sum() }
+fn fuel(x: usize) -> Option<usize> {
+    (x / 3).checked_sub(2)
+}
 
 fn recurse(x: usize) -> usize {
     match fuel(x) {
@@ -11,18 +9,26 @@ fn recurse(x: usize) -> usize {
     }
 }
 
-pub fn part2(raw: &[String]) -> usize { raw.iter().map(|x| int(x)).map(recurse).sum() }
+pub fn run(raw: &[String]) -> (usize, usize) {
+    (
+        raw.iter()
+            .map(|x| x.parse::<usize>().ok().and_then(fuel))
+            .flatten()
+            .sum(),
+        raw.iter()
+            .map(|x| x.parse::<usize>().ok().map(recurse))
+            .flatten()
+            .sum(),
+    )
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test1() {
-        assert_eq!(part1(&read("day01.txt")), 3184233);
-    }
+    use crate::utils::*;
 
     #[test]
-    fn test2() {
-        assert_eq!(part2(&read("day01.txt")), 4773483);
+    fn test() {
+        assert_eq!(run(&read("day01.txt")), (3184233, 4773483));
     }
 }
