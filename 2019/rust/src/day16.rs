f13eda63223ad6a0f115c57e9758742099505060
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2021-07-18 15:03:59 -0400

    More reasonable day 16.
    Signed-off-by: Chaichontat Sriworarat <chaichontat.s@duke.edu>

--- 2019/rust/src/day16.rs
+++ 2019/rust/src/day16.rs
@@ -7,6 +7,10 @@ use super::utils::*;
 
 const ZERO: i32 = 48;
 
+fn gen(x: &[u32]) -> u32 {
+    x.iter().fold(0, |sum, y| 10 * sum + *y)
+}
+
 fn fft(start: &[i32], step: u16) -> Vec<i32> {
     let mut cs = start.to_owned();
     let n = cs.len() - 1;
@@ -37,54 +41,68 @@ fn fft(start: &[i32], step: u16) -> Vec<i32> {
 
 pub fn part1(raw: &[AsciiString]) -> u32 {
     // Need to prepend 0 for a much simpler implementation of the add/subtract alternation.
-    let mut cs = iter::once(0_i32)
+    let cs = iter::once(0_i32)
         .chain(raw[0].into_iter().map(|&x| x as i32 - ZERO))
         .collect_vec();
 
     let ans = fft(&cs, 100);
-    ans[1..9].iter().fold(0, |sum, &i| 10 * sum + i as u32)
+    gen(&ans[1..9].iter().map(|&x| x as u32).collect_vec())
 }
 
-fn comb99mod10(k: u32, b: u32) -> u32 {
-    if b == 5 {
-        match k {
-            l if l % 125 == 0 => 1,
-            l if l % 125 == 25 => 25,
-            _ => 0,
-        }
-    } else if b == 2 {
-        match k {
-            l if (0..=28).step_by(4).contains(&(l % 128)) => 1,
-            _ => 0,
-        }
-    } else {
-        unreachable!()
-    }
-}
+/// Observation: all known offsets are typically toward the end of the sequence.
+///     https://www.reddit.com/r/adventofcode/comments/ebf5cy/2019_day_16_part_2_understanding_how_to_come_up/fb4awi4/
+///     When the offset n is more than ½ the length of the sequence, the FFT of that digit
+///     is the partial sum from that digit to the end (mod 10). Since all numbers are positive,
+///     we don't need to worry about the abs function.
+///
+/// Then, https://www.reddit.com/r/adventofcode/comments/ebqgdu/2019_day_16_part_2_lets_combinatorics/
+/// By decomposing the input digit-by-digit, we can get the relation between the input and output as a combinatorics function.
+/// Therefore, the final output is
+/// > outputₙ mod 10 ≡ (inputₙ·₉₉C₀ + inputₙ₊₁·₁₀₀C₁ + inputₙ₊₂·₁₀₁C₂ + ⋯) mod 10
+/// where n extends back to the diagonal.
+///
+///  Using the Chinese Remainder Theorem, x mod 10 ≡ (5x mod 2 - 4x mod 5) mod 10.
+///
+///  https://github.com/Voltara/advent2019-fast/blob/master/src/day16.cpp
+///  Most of the speedup has to do with C(k+99, k) mod 10
+///     C(k+99, k) % 5 = 1 ; k % 125 = 0
+///                    = 4 ; k % 125 = 25
+///                    = 0 ; otherwise
+///     C(k+99, k) % 2 = 1 ; k % 128 = 0,4,8,12,16,20,24,28
+///                    = 0 ; otherwise
 
-fn find_hidden(s: &[i32]) -> Vec<i32> {
-    let mut s = s.to_owned();
-    for _ in 0..100 {
-        let mut cumsum = s.iter().sum::<i32>();
-        for x in s.iter_mut() {
-            let val = *x;
-            *x = cumsum % 10;
-            cumsum -= val;
-        }
-    }
-    s
-}
-
-// https://www.reddit.com/r/adventofcode/comments/ebqgdu/2019_day_16_part_2_lets_combinatorics/
 pub fn part2(raw: &[AsciiString]) -> usize {
-    let cs = std::iter::repeat(raw[0].into_iter().map(|&x| x as i32 - ZERO))
-        .take(10000)
-        .flatten()
+    let cs = raw[0]
+        .into_iter()
+        .map(|&x| x as u32 - ZERO as u32)
         .collect_vec();
-    let offset = cs[0..7].iter().fold(0, |sum, &i| 10 * sum + i as usize);
+    let n = cs.len();
+    let offset = gen(&cs[0..7].iter().map(|&x| x as u32).collect_vec()) as usize;
 
-    let ans = find_hidden(&cs[offset..]);
-    ans[0..8].iter().fold(0, |sum, &i| 10 * sum + i as usize)
+    let t = 10000 * n;
+
+    let mut out = 0u32;
+    for d in offset..offset + 8 {
+        let mut sum = 0u32;
+        for i in (0..t - d).step_by(128) {
+            for j in (0..=28).step_by(4) {
+                if i + j < t - d {
+                    sum += 5 * cs[(d + i + j) % n] as u32;
+                }
+            }
+        }
+
+        for i in (0..t - d).step_by(125) {
+            sum += 6 * cs[(d + i) % n] as u32;
+            if i + 25 < t - d {
+                sum += 4 * cs[(d + i + 25) % n] as u32;
+            }
+        }
+
+        out = 10 * out + sum % 10;
+    }
+
+    out as usize
 }
 
 #[cfg(test)]
