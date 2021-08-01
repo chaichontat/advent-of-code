e627230a8d68154559738d9bc16ce28ba5134cce
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2021-08-01 11:57:07 -0400

    Update day 24.
    Signed-off-by: Chaichontat Sriworarat <chaichontat.s@duke.edu>

--- 2019/rust/src/y2018/day24.rs
+++ 2019/rust/src/y2018/day24.rs
@@ -3,8 +3,8 @@ use std::{cmp::Reverse,
           mem,
           str::{from_utf8, FromStr}};
 
-use itertools::{izip, Itertools};
-use rayon::{iter::plumbing::UnindexedConsumer, prelude::*};
+use itertools::Itertools;
+use rayon::prelude::*;
 use regex::Regex;
 use safe_arch::bit_lowest_set_reset_u32;
 use strum_macros::EnumString;
@@ -39,8 +39,8 @@ impl Default for Attack {
 pub struct Group {
     team:       Team,
     units:      u32,
-    initiative: u32,
-    target:     Option<usize>,
+    initiative: u8,
+    target:     Option<u8>,
     attack:     Attack,
     hp:         u32,
     dp:         u32,
@@ -78,7 +78,7 @@ impl Group {
         self.ep = self.calc_ep();
     }
 
-    pub fn set_target(&mut self, target: Option<usize>) {
+    pub fn set_target(&mut self, target: Option<u8>) {
         self.target = target;
     }
 }
@@ -134,17 +134,17 @@ impl Universe {
         self.groups.iter().map(|g| g.units).sum()
     }
 
-    fn set_units(&mut self, initiative: usize, units: u32) {
+    fn set_units(&mut self, initiative: u8, units: u32) {
         if units == 0 {
-            self.alive[self.groups[initiative].team as usize] &= !(1 << initiative);
+            self.alive[self.groups[initiative as usize].team as usize] &= !(1 << initiative);
         }
-        self.groups.get_mut(initiative).unwrap().set_units(units);
+        self.groups.get_mut(initiative as usize).unwrap().set_units(units);
         self.rearrange_select_order(initiative);
         debug_assert!(self.is_sound());
     }
 
-    fn rearrange_select_order(&mut self, changed: usize) {
-        let defender = self.groups[changed];
+    fn rearrange_select_order(&mut self, changed: u8) {
+        let defender = self.groups[changed as usize];
         let mut i = self
             .select_ord
             .iter()
@@ -152,20 +152,16 @@ impl Universe {
             .unwrap()
             .0;
 
-        while i + 1 < N_UNITS && defender.ep < self.groups[self.select_ord[i + 1]].ep {
-            let (fst, snd) = self.select_ord.split_at_mut(i + 1);
-            mem::swap(&mut fst[i], &mut snd[0]);
-            i += 1;
-        }
-
         while i + 1 < N_UNITS
-            && defender.ep == self.groups[self.select_ord[i + 1]].ep
-            && defender.initiative as usize > self.select_ord[i + 1]
+            && (defender.ep < self.groups[self.select_ord[i + 1]].ep
+                || (defender.ep == self.groups[self.select_ord[i + 1]].ep
+                    && defender.initiative as usize > self.select_ord[i + 1]))
         {
             let (fst, snd) = self.select_ord.split_at_mut(i + 1);
             mem::swap(&mut fst[i], &mut snd[0]);
             i += 1;
         }
+
         debug_assert!(self.is_sound());
     }
 
@@ -173,6 +169,7 @@ impl Universe {
         let mut temp = self.select_ord;
         temp.sort_unstable_by_key(|i| fn_select_ord(&self.groups, i));
         self.select_ord = temp;
+        debug_assert!(self.is_sound());
     }
 
     fn boost_immunity(&self, val: u32) -> Self {
@@ -183,12 +180,13 @@ impl Universe {
             out.groups[im.trailing_zeros() as usize].set_dp(old_dp + val);
             im = bit_lowest_set_reset_u32(im);
         }
-
         out.sort_select_order();
         debug_assert!(out.is_sound());
         out
     }
 
+    /// Check if invariants within the struct is broken.
+    /// Check if selection order is sorted, effective power = units * dp, consistent alive state.
     fn is_sound(&self) -> bool {
         assert!(self.select_ord.is_sorted_by_key(|x| fn_select_ord(&self.groups, x)));
         assert!(self.groups.iter().all(|&g| g.ep == g.calc_ep()));
@@ -204,7 +202,7 @@ impl Universe {
     }
 }
 
-fn fn_select_ord(groups: &[Group], i: &usize) -> (Reverse<u32>, u32) {
+fn fn_select_ord(groups: &[Group], i: &usize) -> (Reverse<u32>, u8) {
     (Reverse(groups[*i].ep), groups[*i].initiative)
 }
 
@@ -249,7 +247,7 @@ fn miniparse(groups: &mut [Group], lines: &[String], team: Team) -> Option<bool>
             attack,
             dp,
             ep: n * dp,
-            initiative: N_UNITS as u32 - initiative,
+            initiative: N_UNITS as u8 - initiative as u8,
             weak,
             immune,
             target: None,
@@ -298,7 +296,7 @@ fn choose_target(uni: &Universe) -> (Groups, Bits) {
             let idx = cands.trailing_zeros() as usize;
             if uni.groups[idx].ep > target_max_ep {
                 target_max_ep = uni.groups[idx].ep;
-                target = Some(idx);
+                target = Some(idx as u8);
             }
             cands = bit_lowest_set_reset_u32(cands);
         }
@@ -325,7 +323,7 @@ fn run(mut uni: Universe) -> Option<Universe> {
             attacking = bit_lowest_set_reset_u32(attacking); // Pop
 
             let attacker = uni.groups[idx as usize];
-            let temp_defender = uni.groups[attacker.target.unwrap()];
+            let temp_defender = uni.groups[attacker.target.unwrap() as usize];
 
             debug_assert!(attacker.units > 0);
             debug_assert!(temp_defender.units > 0);
@@ -342,14 +340,12 @@ fn run(mut uni: Universe) -> Option<Universe> {
                 temp_defender.units.saturating_sub(real_ep / temp_defender.hp),
             );
 
-            let defender = &uni.groups[attacker.target.unwrap()];
-            if defender.units == 0 {
-                is_infinite_loop = false;
-                attacking &= !(1 << defender.initiative);
-            }
-
+            let defender = &uni.groups[attacker.target.unwrap() as usize];
             if old_units != defender.units {
                 is_infinite_loop = false;
+                if defender.units == 0 {
+                    attacking &= !(1 << defender.initiative);
+                }
             }
 
             debug_assert!(uni.is_sound());
