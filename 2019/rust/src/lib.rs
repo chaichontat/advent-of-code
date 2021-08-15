f647f05bb6d12df3b35d31f0099704278738428c
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2021-08-15 10:46:42 -0400

    Fix lib.
    Signed-off-by: Chaichontat Sriworarat <chaichontat.s@duke.edu>

--- 2019/rust/src/lib.rs
+++ 2019/rust/src/lib.rs
@@ -16,7 +16,7 @@ pub mod utils;
 pub mod y2019 {
     pub mod day01;
     pub mod day02;
-    pub mod day03;
+    // pub mod day03;
     pub mod day04;
     pub mod day05;
     pub mod day06;
@@ -34,7 +34,7 @@ pub mod y2019 {
     pub mod day18;
     pub mod day19;
     pub mod day20;
-    pub mod day21;
+    // pub mod day21;
     pub mod day22;
     pub mod day23;
     pub mod day24;
