16c34e6ad2da4f03d8e44b5561af260e7ab36194
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2021-09-05 12:54:32 -0400

    Add NDArrayInt
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- python/y2020/utils.py
+++ python/y2020/utils.py
@@ -3,6 +3,11 @@ from __future__ import annotations  # Until Python 3.10
 from pathlib import Path
 from typing import Literal, Optional, Sequence, overload
 
+import numpy as np
+import numpy.typing as npt
+
+NDArrayInt = npt.NDArray[np.int_]
+
 
 @overload
 def load(path: Path | str, *, parseint: Literal[True], split: Optional[str] = ...) -> list[int]:
