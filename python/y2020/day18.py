633bb684c7945bba539bf91aff89125001429ac8
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2021-09-05 12:49:45 -0400

    Make div impossible
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- python/y2020/day18.py
+++ python/y2020/day18.py
@@ -1,20 +1,22 @@
 #%%
+from __future__ import annotations
+
 import re
 from operator import add, mul
-from typing import Iterable
+from typing import Iterator, NoReturn
 
 from utils import load
 
 raw = load("day18.txt")
 
 
-def stepin(it: Iterable[str]) -> int:
+def stepin(it: Iterator[str]) -> int:
     if (x := next(it)) == "(":
         return parse(it)
     return int(x)
 
 
-def parse(it: Iterable[str]) -> int:
+def parse(it: Iterator[str]) -> int:
     cmds = {"+": add, "*": mul}
     curr = stepin(it)
     try:
@@ -25,7 +27,7 @@ def parse(it: Iterable[str]) -> int:
     return curr
 
 
-def test1():
+def test1() -> None:
     assert sum(parse(iter(line.replace(" ", ""))) for line in raw) == 29839238838303
 
 
@@ -33,32 +35,37 @@ def test1():
 class Swap(int):
     """Swap functions for *, + while maintaining order of operations."""
 
-    def __new__(cls, value):
+    def __new__(cls, value: int) -> Swap:
         return super(cls, cls).__new__(cls, value)
 
-    def __add__(self, other):
+    def __add__(self, other: int) -> Swap:
         return self.__class__(super(Swap, self).__mul__(other))
 
-    def __sub__(self, other):
-        return self.__class__(super(Swap, self).__div__(other))
+    def __sub__(self, other: int) -> NoReturn:
+        raise NotImplementedError
 
-    def __mul__(self, other):
+    def __mul__(self, other: int) -> Swap:
         return self.__class__(super(Swap, self).__add__(other))
 
-    def __div__(self, other):
-        return self.__class__(super(Swap, self).__sub__(other))
+    def __truediv__(self, other: int) -> NoReturn:
+        raise NotImplementedError
 
-    def __repr__(self):
+    def __floordiv__(self, x: int) -> NoReturn:
+        raise NotImplementedError
+
+    def __repr__(self) -> str:
         return f"Swap({int(self)})"
 
 
 def run_swap(x: str) -> int:
     x = x.translate(str.maketrans("*+", "+*"))
-    return int(eval(re.sub(r"(\d+)", r"Swap(\1)", x)))
+    return int(eval(re.sub(r"([0-9]+)", r"Swap(\1)", x)))
 
 
-def test2():
+def test2() -> None:
     assert sum(run_swap(x) for x in raw) == 201376568795521
 
+
+# %%
 
 # %%
